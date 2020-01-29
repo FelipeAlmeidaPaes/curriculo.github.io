d6116c5dd741d797e5c15e3cf8f7fe81b22751dc
Felipe Almeida <felipe.almeida.paes22@gmail.com>
2020-01-29 10:15:30 -0300

    activate gzip on htaccess

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,14 @@ var connect = require('gulp-connect-php');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
 var postcss = require('gulp-postcss');
+var gzip = require('gulp-gzip');
 var bs = require('browser-sync').create();
 var style = '';
 
 var paths = {
   php: ['*.php'],
-  css: ['css/*.css']
+  css: ['css/*.css'],
+  js: ['js/*.js']
 }
 
 gulp.task('style', function () {
@@ -22,17 +24,27 @@ gulp.task('style', function () {
 exports.style = style;
 
 gulp.task('watch', function () {
-  gulp.watch(paths.css, gulp.series('style', 'css'))
+  gulp.watch(paths.css, gulp.series('style', 'css', 'reload'));
+  gulp.watch(paths.php, gulp.series('reload'));
+  gulp.watch(paths.js, gulp.series('zipJs', 'reload'));
 })
+
 
 gulp.task('css', function () {
   gulp.src(paths.css)
     .pipe(postcss([
       require('autoprefixer')
     ]))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gzip())
     .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('zipJs', function () {
+  gulp.src(paths.js)
+    .pipe(gzip())
+    .pipe(gulp.dest('js/'));
+})
 
 gulp.task('autoprefixer', () =>
   gulp.src(paths.css)
