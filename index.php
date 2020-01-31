d7e9c5e38ac120709c5ec6f093d172d2263a1347
Felipe Almeida <felipe.almeida.paes22@gmail.com>
2020-01-31 09:42:14 -0300

    change on analytics

--- index.php
+++ index.php
@@ -36,16 +36,6 @@
   </script>
   <!-- End Google Tag Manager -->
 
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157442801-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-157442801-1');
-</script>
-
 </head>
 
 <body>
